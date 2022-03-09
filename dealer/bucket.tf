090c078a9f49c04568d51a9931d1ac24c3136881
Jose Ledesma <=>
2022-03-09 14:20:35 -0600

    Dealer Project Part 2

--- dealer/bucket.tf
+++ dealer/bucket.tf
@@ -10,14 +10,22 @@ provider "aws" {
 
 resource "aws_s3_bucket" "bucket1" {
   bucket = var.bucket
-  acl = "public.read"
-  website {
-    index_document = "index.html"
+}
+
+resource "aws_s3_bucket_acl" "bucket1acl" {
+  bucket = aws_s3_bucket.bucket1.id
+  acl    = "public.read"
+}
+
+resource "aws_s3_bucket_website_configuration" "bucket1website" {
+  bucket = aws_s3_bucket.bucket1.id
+  index_document {
+    suffix = "index.html"
   }
 }
 
 resource "aws_s3_bucket_object" "object1" {
-  bucket = aws_s3_bucket.bucket1.bucket
+  bucket = aws_s3_bucket.bucket1.id
   key = "index.html"
   acl = "public.read"
   content = var.content
