f8730856bfb8c49bb037678b3c38486e7170a109
Jose Ledesma <=>
2022-03-09 14:09:41 -0600

    Dealer Project

---
+++ dealer/vars.tf
@@ -0,0 +1,14 @@
+variable "bucket" {
+  type = string
+  default = "mybucket"
+}
+
+variable "region" {
+  type = string
+  default = "us-east-2"
+}
+
+variable "content" {
+  type = string
+  default = "Hello World!"
+}
